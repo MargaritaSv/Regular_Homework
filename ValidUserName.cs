820d9fb7afac4ebf51d5a5552125a64bbd023c75
MargaritaSv <margarita.esti@gmail.com>
2015-10-15 15:28:50 +0300

    Valid Usernames

---
+++ ValidUserName.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class ValidUserName
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        string valid = @"\b[a-zA-Z]\w{3,25}\b";
+
+        Regex rg = new Regex(valid);
+        MatchCollection mts = rg.Matches(input);
+        int max = int.MinValue;
+        int fIndex = 0;
+        int secIndex = 0;
+
+        for (int i = 0; i < mts.Count - 1; i++)
+        {
+            int temp = mts[i].Length + mts[i + 1].Length;
+
+            if (temp > max)
+            {
+                max = temp;
+                fIndex = i;
+                secIndex = i + 1;
+            }
+        }
+        Console.WriteLine(mts[fIndex]);
+        Console.WriteLine(mts[secIndex]);
+    }
+}
